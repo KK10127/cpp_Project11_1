baef0c8302b8ce646bddc785ee813c281fd10f55
Jay Montoya <jayjoplin9898@gmail.com>
2019-03-09 13:19:13 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+/*
+ * Design a class Numbers that can be used to translate whole dollar amounts in
+ * the range 0 through 9999 into an english description of the number. For example,
+ * the number 713 would be translated into the string "seven hundred thirteen" and
+ * 8203 would be translated into eight thousand two hundred three.
+ *
+ * The class should have a single integer member variable.
+ *
+ * int number;
+ *
+ * and a collection of statis string members that specify how to translate key dollar
+ * amounts into the desired format. For example, you might use static strings such
+ * as
+ *
+ * string lessThan20[ ] =
+ *  { "zero", "one", "...", "eighteen", "nineteen" };
+ * string hundred = "hundred";
+ * string thousand = "thousand";
+ *
+ * The class should have a constructor that accepts a non-negative integer and uses it
+ * to initialize the Numbers object. It should have a member function print() that prints
+ * the English description of the Numbers object. Demonstrate the class by writing a main
+ * program that asks the user to enter a number in the proper range and then prints out
+ * its English description.
+ *
+ * @author Jay Montoya
+ * @version Chapter 11 Project 1
+ *
+ */
+int main() {
+
+
+    return 0;
+}
